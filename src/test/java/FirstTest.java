1d11b6b6b68f9d5339f3d2beabd626ff2d9843f9
mar-y-m <JJJunior22>
2020-11-12 19:07:32 +0300

    start

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,33 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class FirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
+        System.setProperty("webdriver.gecko.driver","C:\\Users\\Garuda\\Documents\\selenium\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 10);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void MyFirstTest(){
+        driver.get("http://www.google.com/");
+        System.out.println(driver.getTitle());
+    }
+
+    @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
