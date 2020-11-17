2b4fa382710cc90f462c96888772f6b221494cb0
Marina Minaeva (TSR) <marina.minaeva@t-systems.com>
2020-11-17 21:57:49 +0100

    add price and half city

--- src/test/java/CianTest.java
+++ src/test/java/CianTest.java
@@ -2,6 +2,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static org.openqa.selenium.By.*;
@@ -15,6 +16,7 @@ public class CianTest {
     public void start(){
         System.setProperty("webdriver.gecko.driver","C:\\workspace\\geckodriver.exe");
         driver = new FirefoxDriver();
+        driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
     }
 //    @After
@@ -33,8 +35,13 @@ public class CianTest {
 
         //2. отметить правильный чекбокс тип жилья
         //3. задать правильное количество комнат
+
         //4. задать правильный рендж цен
+        setPriceRange("f","500000");
+
         //5. выбрать правильный город
+        setCity("Воронеж");
+
         //6. нажать поиск
         //7. выбрать результаты с первой страницы из дива оффер
         //7,5. сохранить результаты в эксельку
@@ -49,6 +56,24 @@ public class CianTest {
         WebElement correctType = driver.findElement(By.ByLinkText.linkText(requiredType));
             correctType.click();
         }
+    }
+    private void setRoomsNumber(){
+
+    }
+
+    private void setPriceRange(String from, String to){
+        WebElement priceFilterButton = driver.findElement(xpath("//div[@data-name='Filters']//div[@data-mark='FilterPrice']/button"));
+        priceFilterButton.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(xpath("//div[@data-name='Filters']//div[@data-mark='FilterPrice']/div[contains(@class,'dropdown')]")));
+        driver.findElement(By.xpath("//div[@data-name='Filters']//div[@data-mark='FilterPrice']//input[@placeholder='от']")).sendKeys(from);
+        driver.findElement(By.xpath("//div[@data-name='Filters']//div[@data-mark='FilterPrice']//input[@placeholder='до']")).sendKeys(to);
+    }
+
+    private void setCity(String city){
+        WebElement cityInputField = driver.findElement(By.xpath("//div[@data-name='Filters']//div[@data-mark='FilterGeo']//input"));
+        cityInputField.sendKeys(city);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@data-name='SuggestionPopup']")));
+        //driver.findElements(ByLinkText.linkText(city)).get(0).click();
     }
 
 }
